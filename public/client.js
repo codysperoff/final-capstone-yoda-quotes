a0b51148e61b4003b21bd0d78a9f2b321b449da8
Cody Speroff <cnsperoff@gmail.com>
2017-06-19 20:06:45 -0500

    removed transparent background, added starwars yellow color

--- public/client.js
+++ public/client.js
@@ -6,26 +6,7 @@ $(document).on('submit', "#search-form", function (key) {
 
 });
 
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
 
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function (event) {
-    if (!event.target.matches('.dropbtn')) {
-
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-}
 
 //function to get results from api
 function getResults(query) {
